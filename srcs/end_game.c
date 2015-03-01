d7a1f6472be0ca3ad84409c016f7eb016e8d920b
alegent <alegent@student.42.fr>
2015-03-01 11:52:30 +0100

    norme

--- srcs/end_game.c
+++ srcs/end_game.c
@@ -6,7 +6,7 @@
 /*   By: chaueur <chaueur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 16:31:29 by alegent           #+#    #+#             */
-/*   Updated: 2015/03/01 04:32:34 by chaueur          ###   ########.fr       */
+/*   Updated: 2015/03/01 11:51:37 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,18 @@ int						check_win_value(int value)
 	win = WIN_VALUE;
 	if (win < 8)
 	{
-		return FAILURE;
+		return (FAILURE);
 		error("2042", "Please insert a higher win value.");
 	}
 	if ((win != 0) && ((win & (win - 1)) == 0))
 	{
 		if (win == (unsigned)value)
-			return SUCCESS;
-		return FAILURE;
+			return (SUCCESS);
+		return (FAILURE);
 	}
 	else
 	{
-		return FAILURE;
+		return (FAILURE);
 		error("2042", "Win value must be power of 2.");
 	}
-		
 }

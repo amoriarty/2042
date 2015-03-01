149157c05857187d160f97ffd249d29864806eb6
alegent <alegent@student.42.fr>
2015-03-01 11:34:52 +0100

    no probem

--- srcs/rush2042.c
+++ srcs/rush2042.c
@@ -6,53 +6,37 @@
 /*   By: chaueur <chaueur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 16:22:25 by alegent           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2015/02/28 19:30:03 by chaueur          ###   ########.fr       */
-=======
-/*   Updated: 2015/03/01 04:17:19 by chaueur          ###   ########.fr       */
->>>>>>> 51533f18406ecff37e9777d0ebe89ab4a769c808
+/*   Updated: 2015/03/01 11:34:07 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush2042.h"
 
-int						rush2042(int **map)
+int						verif_map(int **map)
 {
-	int					tmp;
+	int					x;
+	int					y;
 
-<<<<<<< HEAD
 	x = -1;
 	while (++x < 4)
 	{
 		y = -1;
 		while (++y < 4)
 		{
-			if ((*map)[x][y] == 0)
+			if (map[x][y] == 0)
 				return (TRUE);
 		}
 	}
 	return (FALSE);
 }
 
-int					rush2042(int ***map)
+int						rush2042(int **map)
 {
 	int				tmp;
 
-	if (verif_map(map) == FALSE)
-		failure();
-	tmp = getch();
-	if (tmp == DOWN_KEY)
-	{
-		down_deplacement(map);
-		create_case(map);
-	}
-	
-/*
-=======
 	tmp = getch();
 	if (tmp == DOWN_KEY)
 		down_move(map);
->>>>>>> 51533f18406ecff37e9777d0ebe89ab4a769c808
 	else if (tmp == UP_KEY)
 		up_move(map);
 	else if (tmp == LEFT_KEY)
